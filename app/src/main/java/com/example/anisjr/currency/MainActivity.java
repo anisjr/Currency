0524272bf80a04287c83f8d2c6a48cc794e37599
Anis JRIBI <anisjribi@gmail.com>
2015-03-02 23:06:03 +0100

    Launch the settings activity

--- app/src/main/java/com/example/anisjr/currency/MainActivity.java
+++ app/src/main/java/com/example/anisjr/currency/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -36,6 +36,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
